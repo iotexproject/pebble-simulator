856fe998b218734e66187e315f1b5b23d2b74d59
raullenchai <raullenchai@gmail.com>
2020-09-19 19:43:36 -0700

    update

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # pebble-simulator
+The simulator for producing pebble data.
 
 ## Dependencies
-. linux  bash 
-. openssl
+- linux bash
+- openssl
 
 ## Run
-./pebbleData.sh  
+`./simulator.sh`
 
-This produces 30 data points at random.
+This, by default, produces 30 data points for all 8 sensors at random. The output is written to pebble.dat which looks like below.
+```
+{"message":{"SNR":187,"VBAT":4.36002,"gas_resistance":1173,"temperature":16.21609,"pressure":515.32678,"humidity":31.51630,"temperature":76.22284,"gyroscope":[6,-1214,8],"accelerometer":[937,1214,1491],"timestamp":"85000"},"signature_r":"00fb7f17b6f524a684ac392eb47761bd1f994fd0a1f92d227a263e3ea981d2007e02204","signature_s":""}
+```
 
 ## Files
-tracker01.key    ECDSA 签名私钥与设备中内置的一致
-pebbleData.sh    simulator
-pebbleData.dat   the data porduced (in JSON format)
+tracker01.key - ECDSA private key used by the simulator
+
+simulator.sh - The simulator
+
+pebble.dat - The data points produced which are in JSON format
--- simulator.sh
+++ simulator.sh
@@ -39,10 +39,10 @@ CountPkg=30
 
 getTime()
 {
-	current=`date "+%Y-%m-%d %H:%M:%S"`  
-	timeStamp=`date -d "$current" +%s`   
+	current=`date "+%Y-%m-%d %H:%M:%S"`
+	timeStamp=`date -d "$current" +%s`
 	# ms
-	currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
+	currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
 	echo $currentTimeStamp
 }
 
@@ -67,7 +67,7 @@ SetMode()
 	;;
      4)
        env_mode=${mode_sel[$key]}
-	;;  
+	;;
      5)
        light_mode=${mode_sel[$key]}
 	;;
@@ -88,54 +88,54 @@ SetMode()
      case $1 in
      1)
        echo "Please input SNR data end with enter( eg. 0 ) :"
-       read str      
+       read str
        SNR=$str
 	;;
      2)
        echo "Please input VBAT data end with enter( eg. 4.145454406738281 ) :"
-       read str      
+       read str
        VBAT=$str
 	;;
      3)
        echo "Please input GPS data end with enter( eg. 60.500525 13.886719 ) :"
-       read str      
+       read str
        GPS[0]=$(echo $str|awk '{print  $1 }')
-       GPS[1]=$(echo $str|awk '{print  $2 }') 
-	;;     
+       GPS[1]=$(echo $str|awk '{print  $2 }')
+	;;
      4)
        echo "Please input env data end with enter(gas_resistance temperature pressure humidity)( eg. 5312190 31.838768005371094 1006.739990234375 51.03300094604492 ):"
-       read str      
+       read str
        env_mode[0]=$(echo $str|awk '{print  $1 }')
        env_mode[1]=$(echo $str|awk '{print  $2 }')
        env_mode[2]=$(echo $str|awk '{print  $3 }')
        env_mode[3]=$(echo $str|awk '{print  $4 }')
-	;;   
+	;;
      5)
        echo "Please input light data end with enter( eg. 53.85771942138672 ) :"
-       read str      
+       read str
        LIGHT=$str
 	;;
      6)
        echo "Please input gyroscope data end with enter( eg. -2 9 12 ) :"
-       read str      
+       read str
        gyr_mode[0]=$(echo $str|awk '{print  $1 }')
-       gyr_mode[1]=$(echo $str|awk '{print  $2 }') 
-       gyr_mode[2]=$(echo $str|awk '{print  $3 }') 
+       gyr_mode[1]=$(echo $str|awk '{print  $2 }')
+       gyr_mode[2]=$(echo $str|awk '{print  $3 }')
 	;;
      7)
        echo "Please input accelerometer data end with enter( eg. -38 166 8270 ) :"
-       read str      
+       read str
        accel_mode[0]=$(echo $str|awk '{print  $1 }')
-       accel_mode[1]=$(echo $str|awk '{print  $2 }') 
-       accel_mode[2]=$(echo $str|awk '{print  $3 }') 
-	;; 
+       accel_mode[1]=$(echo $str|awk '{print  $2 }')
+       accel_mode[2]=$(echo $str|awk '{print  $3 }')
+	;;
       8)
        echo "Please input temperature data end with enter( eg. 31.83876 ) :"
-       read str      
+       read str
        temp=$str
 	;;
-     esac     
-     fi  
+     esac
+     fi
 }
 
 
@@ -145,9 +145,9 @@ SensorMenu()
 	while true
         do
 		printf '\033\143'
-		echo "Which sensor to set, Enter number to choose."
+		echo "Enter number to select a sensor to config"
 		echo ""
-		echo " Sensor             mode              data"
+		echo " Sensor             Mode              Default Data"
 		echo ""
 		echo " 1. SNR             $snr_mode         $SNR"
 		echo " 2. VBAT            $vbat_mode        $VBAT"
@@ -155,23 +155,24 @@ SensorMenu()
 		echo " 4. ENV             $env_mode         ${ENV[0]},${ENV[1]},${ENV[2]}"
 		echo " 5. LIGHT           $light_mode       $LIGHT"
 		echo " 6. gyroscope       $gyr_mode         ${gyr[0]},${gyr[1]},${gyr[2]}"
-		echo " 7. accelerometer   $accel_mode       ${accel[0]},${accel[1]},${accel[2]}" 
-                echo " 8. temperature     $temp_mode        $temp"	
-		echo " 9. Main menu." 
+		echo " 7. accelerometer   $accel_mode       ${accel[0]},${accel[1]},${accel[2]}"
+    echo " 8. temperature     $temp_mode        $temp"
+    echo "---------------------------------------------------------------------------"
+		echo " 9. Main menu"
 		read -n 1 key
-		if [ "$key" == "9" ];then 
+		if [ "$key" == "9" ];then
 			break
-		else	
-			SetMode $key 
-		fi 
-	done		 
+		else
+			SetMode $key
+		fi
+	done
 }
 RandomInt()
 {
   local min=$1
   local max=$2
   local random=$( echo "${min}+${RANDOM}/${RANDOM_MAX_INT}*(${max}-${min}+1)" | bc -l )
-  echo -n ${random%.*}    
+  echo -n ${random%.*}
 }
 RandomFloat()
 {
@@ -181,7 +182,7 @@ RandomFloat()
   local max_int=$3
   local random=$( echo "${RANDOM}/${RANDOM_MAX_INT}*(${max_int}^${precision}) + ${RANDOM}/${RANDOM_MAX_INT} " | bc -l )
   printf "%.${scale}f" ${random}
-	
+
 }
 
 NextData()
@@ -191,7 +192,7 @@ NextData()
          SNR=$( RandomInt 0 255 )
      elif [ $snr_mode == "linear" ];then
          let SNR += $STEP
-         if [ $SNR > 255 ];then 
+         if [ $SNR > 255 ];then
              SNR=0
          fi
      fi
@@ -200,33 +201,33 @@ NextData()
          VBAT=$( RandomFloat 1 5 5 )
      elif [ $snr_mode == "linear" ];then
          let VBAT += 1
-         if [ $VBAT > 5 ];then 
+         if [ $VBAT > 5 ];then
              VBAT=0.145454406738281
          fi
-     fi   
+     fi
      # GPS
      if [ $gps_mode == "random" ];then
          gps_m=$( RandomFloat 1 5 60 )
          gps_g=$( RandomInt 0 90 )
-         let gps_g=gps_g*100  
+         let gps_g=gps_g*100
          GPS[0]=$(echo $gps_g+$gps_m |bc -l)
          gps_m=$( RandomFloat 1 5 60 )
 	 gps_g=$( RandomInt 0 180 )
 	 let gps_g=gps_g*100
-         GPS[1]=$(echo $gps_g+$gps_m |bc -l) 
+         GPS[1]=$(echo $gps_g+$gps_m |bc -l)
      elif [ $snr_mode == "linear" ];then
          GPS[0] = $(echo ${GPS[0]} + 2.1000|bc -l)
          GPS[1] = $(echo ${GPS[1]} + 2.1000|bc -l)
-         if [ ${GPS[0]} > 9060 ];then 
+         if [ ${GPS[0]} > 9060 ];then
              GPS[0]=1032.14545
          fi
-         if [ ${GPS[1]} > 18060 ];then 
+         if [ ${GPS[1]} > 18060 ];then
              GPS[1]=10030.14545
          fi
-     fi      
-      # ENV 
+     fi
+      # ENV
       if [ $env_mode == "random" ];then
-         ENV[0]=$( RandomInt 0 8000 )       
+         ENV[0]=$( RandomInt 0 8000 )
          ENV[1]=$( RandomFloat 1 5 80 )
          ENV[2]=$( RandomFloat 1 5 2000 )
          ENV[3]=$( RandomFloat 1 5 100 )
@@ -235,92 +236,92 @@ NextData()
          ENV[1] = $(echo ${ENV[1]} + 2.1000|bc -l)
          ENV[2] = $(echo ${ENV[2]} + 100|bc -l)
          ENV[3] = $(echo ${ENV[3]} + 2.100|bc -l)
-         if [ ${ENV[0]} > 8000 ];then 
+         if [ ${ENV[0]} > 8000 ];then
              ENV[0]=10.14545
          fi
-         if [ ${ENV[1]} > 80 ];then 
+         if [ ${ENV[1]} > 80 ];then
              GPS[1]=10.14545
          fi
-         if [ ${ENV[2]} > 2000 ];then 
+         if [ ${ENV[2]} > 2000 ];then
              ENV[0]=10.14545
          fi
-         if [ ${ENV[3]} > 100 ];then 
+         if [ ${ENV[3]} > 100 ];then
              GPS[1]=10.14545
          fi
-      fi 
+      fi
       # Light
       if [ $light_mode == "random" ];then
          LIGHT=$( RandomFloat 1 5 2000 )
      elif [ $light_mode == "linear" ];then
          LIGHT=$(echo $LIGHT+50.10001|bc -l)
-         if [ $LIGHT > 2000 ];then 
+         if [ $LIGHT > 2000 ];then
              LIGHT=10.14545
          fi
      fi
      # gyroscope
       if [ $gyr_mode == "random" ];then
-         gyr[0]=$( RandomInt 0 15 )       
+         gyr[0]=$( RandomInt 0 15 )
          gyr[1]=$( RandomInt 0 15 )
-         gyr[2]=$( RandomInt 0 15 ) 
+         gyr[2]=$( RandomInt 0 15 )
          flg=$( RandomInt 1 3 )
          let flg=flg-1
          gyr[$flg]=$(echo 0-${gyr[$flg]}|bc -l)
       elif [ $gyr_mode == "linear" ];then
          gyr[0]=$(${gyr[0]} + 1|bc -l)
          gyr[1]=$(${gyr[1]} + 1|bc -l)
-         gyr[2]=$(${gyr[2]} + 1|bc -l) 
-         if [ ${gyr[0]} > 15 ];then 
+         gyr[2]=$(${gyr[2]} + 1|bc -l)
+         if [ ${gyr[0]} > 15 ];then
              gyr[0]=-1
          fi
-         if [ ${ENV[1]} > 15 ];then 
+         if [ ${ENV[1]} > 15 ];then
              gyr[1]=1
          fi
-         if [ ${ENV[2]} > 15 ];then 
+         if [ ${ENV[2]} > 15 ];then
              gyr[2]=-2
-         fi  
-      fi 
-     
+         fi
+      fi
+
      # accelerometer
       if [ $accel_mode == "random" ];then
-         accel[0]=$( RandomInt 0 5000 )       
+         accel[0]=$( RandomInt 0 5000 )
          accel[1]=$( RandomInt 0 5000 )
-         accel[2]=$( RandomInt 0 5000 ) 
+         accel[2]=$( RandomInt 0 5000 )
          flg=$( RandomInt 1 3 )
          let flg=flg-1
          gyr[$flg]=$(echo 0-${accel[$flg]}|bc -l)
       elif [ $gyr_mode == "linear" ];then
          accel[0]=$(${accel[0]} + 20|bc -l)
          accel[1]=$(${accel[1]} + 20|bc -l)
-         accel[2]=$(${accel[2]} + 20|bc -l)      
-         if [ ${gyr[0]} > 5000 ];then 
+         accel[2]=$(${accel[2]} + 20|bc -l)
+         if [ ${gyr[0]} > 5000 ];then
              accel[0]=-10
          fi
-         if [ ${accel[1]} > 5000 ];then 
+         if [ ${accel[1]} > 5000 ];then
              accel[1]=-12
          fi
-         if [ ${accel[2]} > 5000 ];then 
+         if [ ${accel[2]} > 5000 ];then
              accel[2]=500
-         fi  
-      fi 
+         fi
+      fi
       #temperature
       if [ $light_mode == "random" ];then
          temp=$( RandomFloat 1 5 80 )
      elif [ $light_mode == "linear" ];then
          temp=$(echo $temp+2.10001|bc -l)
-         if [ $temp > 80 ];then 
+         if [ $temp > 80 ];then
              temp=10.14545
          fi
-     fi      
+     fi
       let timestp=timestp+5000
 }
 
 GenerateFile()
 {
-    genFile="$(pwd)/pebbleData.dat"
+    genFile="$(pwd)/pebble.dat"
     if [ -a $genFile ];then
     	rm $genFile
     fi
-        
+
     for((integer = 1; integer <= $CountPkg; integer++))
     do
 	NextData
@@ -329,15 +330,15 @@ GenerateFile()
        ecc_str=$(echo $ecc_str|sed  's/ //g')
        sign_r=$(echo ${ecc_str:8:71})
        sign_s=$(echo ${ecc_str:78:-1})
-       echo "{$objMessage,\"signature_r\":\"$sign_r\",\"signature_s\":\"$sign_s\"}" >> $genFile            
+       echo "{$objMessage,\"signature_r\":\"$sign_r\",\"signature_s\":\"$sign_s\"}" >> $genFile
     done
-    
+
 }
 
 NumberofPackages()
 {
 	printf '\033\143'
-	echo "How many packages you want to generate :"	
+	echo "How many packages you want to generate :"
 	read key
         CountPkg=$key
 }
@@ -349,30 +350,30 @@ main()
     while true
     do
         printf '\033\143'
-        echo "         PEBBLE  DATA   GENERATOR"
+        echo "========>> PEBBLE SIMULATOR <<========"
         echo ""
         echo ""
-        echo " 1.  Set Sensor  mode."
+        echo " 1.  Config Sensors"
         echo ""
-        echo " 2.  Generate data."
+        echo " 2.  Set Number of Data Points: $CountPkg"
         echo ""
-        echo " 3.  Count of packages: $CountPkg ."
+        echo " 3.  Run - Produce Data Points"
         echo ""
-        echo " 4.  Exit."
+        echo " 4.  Exit"
         echo ""
         echo "Select:"
         read -n 1 key
         if [ $key == "1" ];then
             SensorMenu
         elif [ $key == "2" ] ;then
-            echo ""
-            GenerateFile
-            echo ""
-            echo "$(pwd)/genFile  generated!"
-            echo ""
-            break
+					NumberofPackages
         elif [ $key == "3" ];then
-            NumberofPackages
+					echo ""
+					GenerateFile
+					echo ""
+					echo "$(pwd)/genFile  generated!"
+					echo ""
+					break
         else
             break
         fi
@@ -380,5 +381,3 @@ main()
 }
 
 main
-
-
