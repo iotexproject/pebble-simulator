7a9ec0e24b7c0117bd3dd9b275bb0899f6dad1f5
wilson <wilsonclarker@gmail.com>
2021-05-15 01:10:29 +0800

    Heartbeat add command params

--- heartbeat.sh
+++ heartbeat.sh
@@ -3,6 +3,8 @@
 MQTT_BROKER_HOST="a11homvea4zo8t-ats.iot.ap-east-1.amazonaws.com"
 MQTT_BROKER_PORT=8883
 
+device_id=$1
+
 strStart="{\"message\":{\"deviceState\":\"online\"}}"
 strRun="{\"message\":{\"deviceState\":\"running\"}}"
 
@@ -11,6 +13,6 @@ while true
 do
 
     mosquitto_pub -t  "device/${device_id}/action/update-state" -m "$strRun" -h $MQTT_BROKER_HOST  --cafile "$(pwd)/AmazonRootCA1.pem" --cert  "$(pwd)/cert.pem" --key  "$(pwd)/private.pem"  --insecure -p $MQTT_BROKER_PORT 
-    sleep 20
+    sleep 2 
 
 done 
