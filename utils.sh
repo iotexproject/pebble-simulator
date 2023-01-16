d5c8224822d839165471b89d7b57d71fd10428fe
Tide <taegukusa@gmail.com>
2023-01-16 14:36:37 +0800

    Update utils.sh
    For security reasons, the testnet is connected by default.

--- utils.sh
+++ utils.sh
@@ -9,10 +9,10 @@ TYPE_DEV_STATUS="3"
 TYPE_DECODE_CONFIRM="0"
 TYPE_DECODE_REG_OVER="1"
 
-MQTT_CERT="$(pwd)/cert_mainnet.pem"
-MQTT_KEY="$(pwd)/private_mainnet.pem"
-NETTYPE="mainnet"
-MQTT_BROKER_HOST="a11homvea4zo8t-ats.iot.ap-east-1.amazonaws.com"
+MQTT_CERT="$(pwd)/cert_testnet.pem"
+MQTT_KEY="$(pwd)/private_testnet.pem"
+NETTYPE="testnet"
+MQTT_BROKER_HOST="a11homvea4zo8t-ats.iot.us-east-1.amazonaws.com"
 MQTT_BROKER_PORT=8883
 
 
